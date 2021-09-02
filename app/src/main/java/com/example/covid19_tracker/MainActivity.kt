6fc9399098384ad75e39f2836bfb458cdf27c695
Tanish <tanishkumar5007@gmail.com>
2021-09-02 18:15:10 +0530

    idk

--- app/src/main/java/com/example/covid19_tracker/MainActivity.kt
+++ app/src/main/java/com/example/covid19_tracker/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.covid19_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.covid19_tracker.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fetchResult()
+
+    }
+
+    @DelicateCoroutinesApi
+    private fun fetchResult() {
+        GlobalScope.launch(Dispatchers.IO) {
+            val response = withContext(Dispatchers.IO){ Client.api.execute() }
+            if(response.isSuccessful){
+                val data = response.body?.string()
+                val result = Gson().fromJson(data, Response::class.java)
+            }
+        }
+
     }
 }
